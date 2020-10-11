f702a0f45449d7f7b2ebdc9a8518761b908d1fea
Anshu kumar shandilya <30520154+ashandilya@users.noreply.github.com>
2020-10-11 03:42:08 +0000

    Android App Files Added with Updated Design

---
+++ app/src/main/java/com/kiwismedia/login_registration_module/Login.java
@@ -0,0 +1,14 @@
+package com.kiwismedia.login_registration_module;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
